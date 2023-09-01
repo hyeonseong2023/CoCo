f2fe4a11210d4ee86914e335a9b8ba38b4cbfd5e
김다운 <128446489+kimdaun98@users.noreply.github.com>
2023-09-01 09:56:55 +0900

    header 업데이트 싹다 정리함
    header

--- frontend/src/component/Header.tsx
+++ frontend/src/component/Header.tsx
@@ -1,47 +1,21 @@
 import React, { useState } from 'react';
 import '../css/Header.css';
-import Logo from '../img/Logo.png';
-import LoginImg from '../img/Login.png';
-import Login from './Login';
 
 type HeaderProps = {};
 
 const Header: React.FC<HeaderProps> = ({}) => {
-  const [isLoginModalOpen, setLoginModalOpen] = useState<boolean>(false);
-
-const openLoginModal = () => {
-  setLoginModalOpen(true);
-  document.body.classList.add('modal-open'); // 페이지 스크롤 막기
-};
-
-const closeLoginModal = () => {
-  setLoginModalOpen(false);
-  document.body.classList.remove('modal-open'); // 페이지 스크롤 복구
-};
-
-  return (
-    <div id="header-container">
-      <div id="header-logo-box">
-        <img src={Logo} alt="로고" />
-      </div>
-      <div id='header-white-space'>
-        <div id="header-login-container">
-          <button id='header-write'> 모집글 작성 </button>
-          <button id='header-login' onClick={openLoginModal}>
-            <img src={LoginImg} alt="로그인" />
-          </button>
-        </div>
-      </div>
-
-      {isLoginModalOpen && (
-        <div className="modal-backdrop">
-          <div className="modal">
-            <div className="modal-content">
-              <Login onClose={closeLoginModal} />
-            </div>
+  return(
+    // 리턴용 컨테이너는 건드리지 말것
+    <div>
+      <div>
+        <div>
+          <div>로고</div>
+          <div>
+            <div>글쓰기</div>
+            <div>로그인</div>
           </div>
         </div>
-      )}
+      </div>
     </div>
   );
 };
