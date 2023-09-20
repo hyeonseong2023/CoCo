79b21779470d717c607124d45c4819b3fc873455
HwangJeongyong <jeongyong62@gmail.com>
2023-09-20 09:23:21 +0900

    morning commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Main from './component/Main';
-import Write from './component/Write';
-import Mypage from './component/Mypage';
-import Check from './component/Check';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Main from "./component/Main";
+import Write from "./component/Write";
+import Mypage from "./component/Mypage";
+import Check from "./component/Check";
 
-import KakaoCallback from './component/Kakao/KakaoCallback';
-import './App.css';
+import KakaoCallback from "./component/Kakao/KakaoCallback";
+import "./App.css";
+import ProjectPage from "./component/Project/ProjectPage";
 
 const App: React.FC = () => {
   return (
     <Router>
-      <div className='main-div'>
+      <div className="main-div">
         <Routes>
           <Route path="/write" element={<Write />} />
           <Route path="/" element={<Main />} /> {/* 기본 경로 */}
           <Route path="/Mypage" element={<Mypage />} /> {/* 마이페이지 경로 */}
           <Route path="/callback/*" element={<Main />} /> {/* 기본 경로 */}
-          <Route path='/Check' element={<Check />} />
-          <Route path='/auth/kakao/callback' element={<KakaoCallback />} />
+          <Route path="/Check" element={<Check />} />
+          <Route path="/auth/kakao/callback" element={<KakaoCallback />} />
+          <Route path="/pp" element={<ProjectPage />} />
         </Routes>
       </div>
     </Router>
--- frontend/src/component/Project/SideBar.tsx
+++ frontend/src/component/Project/SideBar.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
 
-const SideBar = () => {
+const SideBar = ({
+  setSelectedMenu,
+}: {
+  setSelectedMenu: (e: string) => void;
+}) => {
   return (
-    <div>SideBar</div>
-  )
-}
+    <div>
+      <div
+        onClick={() => {
+          setSelectedMenu("Page");
+        }}
+      >
+        페이지
+      </div>
+      <div
+        onClick={() => {
+          setSelectedMenu("Planner");
+        }}
+      >
+        일정관리
+      </div>
+    </div>
+  );
+};
 
-export default SideBar
+export default SideBar;
